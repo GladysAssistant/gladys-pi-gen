140d060a406da59bc187ff3d561567cbdccbebd6
VonOx <vkulak62@gmail.com>
2021-12-16 11:25:55 +0100

    Disable ipv6 on host

--- gladys/03-sys-tweaks/01-run.sh
+++ gladys/03-sys-tweaks/01-run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
 install -m 755 files/rc.local		"${ROOTFS_DIR}/etc/"
+
+on_chroot <<EOF
+echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.eth0.disable_ipv6=1" >> /etc/sysctl.conf
+EOF
